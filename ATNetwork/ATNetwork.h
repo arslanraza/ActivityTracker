c829369f55aa8223af268cddbac1097d4ff527d5
Arslan Raza <arslan@insight.co>
2018-03-15 14:39:04 +1100

    - adds a ATNetwork framework to implement network logic

---
+++ ATNetwork/ATNetwork.h
@@ -0,0 +1,19 @@
+//
+//  ATNetwork.h
+//  ATNetwork
+//
+//  Created by Arslan Raza on 15/3/18.
+//  Copyright © 2018 ArkTechs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATNetwork.
+FOUNDATION_EXPORT double ATNetworkVersionNumber;
+
+//! Project version string for ATNetwork.
+FOUNDATION_EXPORT const unsigned char ATNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATNetwork/PublicHeader.h>
+
+
