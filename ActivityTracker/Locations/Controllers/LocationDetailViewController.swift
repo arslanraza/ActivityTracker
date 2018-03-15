b9e69e2f5a14dd7442f2fa3d75771f92d66d5441
Arslan Raza <arslan@insight.co>
2018-03-15 17:11:27 +1100

    - adds LocationDetailViewController and writes one unit tests for it to check tableView connection

---
+++ ActivityTracker/Locations/Controllers/LocationDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LocationDetailViewController.swift
+//  ActivityTracker
+//
+//  Created by Arslan Raza on 15/3/18.
+//  Copyright © 2018 ArkTechs. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
---
+++ ActivityTrackerTests/Tests/ActivityTracker/LocationDetailViewControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  LocationDetailViewControllerTests.swift
+//  ActivityTrackerTests
+//
+//  Created by Arslan Raza on 15/3/18.
+//  Copyright © 2018 ArkTechs. All rights reserved.
+//
+
+import UIKit
+import Quick
+import Nimble
+@testable import ActivityTracker
+
+class LocationDetailViewControllerTests: QuickSpec {
+  override func spec() {
+    describe("LocationDetail controller") {
+      
+      context("when displayed", {
+        let sut = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LocationDetailViewController") as! LocationDetailViewController
+        let _ = sut.view
+        it("should have a tableView", closure: {
+          expect(sut.tableView).notTo(beNil())
+        })
+      })
+    }
+  }
+}
+
